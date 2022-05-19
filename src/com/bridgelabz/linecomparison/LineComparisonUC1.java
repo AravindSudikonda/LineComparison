50f740c7e53a4dfe5c75369d46ac5b8058d779a8
AravindSudikonda <aravindsudikonda10@example.com>
2022-05-19 17:18:02 +0530

    adde welcome message

---
+++ src/com/bridgelabz/linecomparison/LineComparisonUC1.java
@@ -0,0 +1,15 @@
+package com.bridgelabz.linecomparison;
+
+import java.util.Scanner;
+
+
+public class LineComparisonUC1 {
+
+    public static void main(String[] args) {
+
+        System.out.println("Welcome to line comparision program");
+
+    }
+}
+
+
